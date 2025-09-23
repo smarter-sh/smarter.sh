fba18af39666c5b31aef84ca9f4a8ced0e4dfb65
lpm0073 <lpm0073@gmail.com>
2025-09-23 13:11:05 -0600

    refactor: switch to WebAI template package from envato

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,39 +5,35 @@ import formPlugin from '@tailwindcss/forms'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-     "node_modules/preline/dist/*.js",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "10rem",
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '10rem',
       },
     },
 
     fontFamily: {
-      jost: ["Jost", "sans-serif"],
+      jost: ['Jost', 'sans-serif'],
     },
 
     extend: {
       colors: {
-          primary: colors.orange["600"],
-          "primary-hover": colors.orange["700"],
+        primary: colors.orange['600'],
+        'primary-hover': colors.orange['700'],
 
         default: colors.slate,
       },
     },
   },
-  plugins: [
-    prelinePlugin,
-    formPlugin,
-  ],
+  plugins: [prelinePlugin, formPlugin],
 }
-
