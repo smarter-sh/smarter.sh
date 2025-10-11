847a476c2f2e6c4d4c45021b4eb8efafff53a96d
lpm0073 <lpm0073@gmail.com>
2025-10-11 11:50:41 -0600

    test: setup unit tests

---
+++ jest.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx}'
+  ],
+  // Fix for naming collisions
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/webai/'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/webai/'
+  ]
+};
---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+test('renders app component', async () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Wait for the component to finish loading
+  await waitFor(() => {
+    // Just test that the app renders without crashing
+    expect(document.querySelector('div')).toBeInTheDocument();
+  });
+
+  // Simple test - just verify the app mounted
+  expect(document.body).toBeInTheDocument();
+
+});
