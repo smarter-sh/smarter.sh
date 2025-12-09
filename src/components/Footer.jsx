798830ab5a033940c008afbc7fa68f308b05cf60
lpm0073 <lpm0073@gmail.com>
2025-12-09 11:59:21 -0600

    chore: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="border-t border-white/10 py-6">
       <div className="container flex h-full flex-wrap items-center justify-center gap-4 text-center md:justify-between md:text-start lg:px-20">
         <p className="text-sm font-mono font-medium text-default-400">
-          2023 - {currentYear} © Project: smarter
+          2023 - {currentYear} © <a href="https://lawrencemcdaniel.com" target="_blank" rel="noopener noreferrer">Lawrence P. McDaniel</a>, The Smarter Project
         </p>
         <p className="text-sm font-mono font-medium text-default-400 flex flex-col md:flex-row gap-2">
           <a href="https://www.patreon.com/fullstackwithlawrence" className="hover:underline" target="_blank" rel="noopener noreferrer">
